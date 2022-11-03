5c1f475f31f13e48b2567fa79be5c022d340de3f
Zahari.Kitanov@stp-online.de <Zahari.Kitanov@stp-online.de>
2022-11-04 00:41:57 +0200

    Moved Database into the correct folder.

---
+++ DocumentsApi.Database/DocumentsApiContext.cs
@@ -0,0 +1,15 @@
+﻿using DocumentsApi.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentsApi.Database
+{
+    public class DocumentsApiContext : DbContext
+    {
+        public DocumentsApiContext(DbContextOptions<DocumentsApiContext> context) : base(context)
+        {
+
+        }
+
+        public DbSet<Document> Documents { get; set; }
+    }
+}
---
+++ DocumentsApi.Database/Models/Document.cs
@@ -0,0 +1,13 @@
+﻿namespace DocumentsApi.Database.Models
+{
+    public class Document
+    {
+        public string DocumentName { get; set; }
+
+        public int Version { get; set; }
+
+        public string FileName { get; set; }
+
+        public long FileSize { get; set; }
+    }
+}
