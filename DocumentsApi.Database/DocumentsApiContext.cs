a26c1b5f5b8e12da6bd99307bb279c6fa0e7e913
Zack <Zack@DESKTOP-U475V66>
2022-11-06 06:52:59 +0200

    Added seed data

--- DocumentsApi.Database/DocumentsApiContext.cs
+++ DocumentsApi.Database/DocumentsApiContext.cs
@@ -10,6 +10,16 @@ namespace DocumentsApi.Database
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Document>()
+                        .HasData(
+                         new Document { DocumentName = "document_instructions", FileName = "document_instructions.docx", FileSize = 2097152 },
+                         new Document { DocumentName = "advanced_javascript_practices", FileName = "advanced_javascript_practices.pdf", FileSize = 12097152 },
+                         new Document { DocumentName = "C# 8.0 and .NET Core 3.0", FileName = "C# 8.0 and .NET Core 3.0.docx", FileSize = 22097152 },
+                         new Document { DocumentName = "C# in Depth", FileName = "C# in Depth.pdf", FileSize = 32097152 });
+        }
+
         public DbSet<Document> Documents { get; set; }
     }
 }
--- DocumentsApi.Database/Models/Document.cs
+++ DocumentsApi.Database/Models/Document.cs
@@ -4,10 +4,13 @@
     {
         public string DocumentName { get; set; }
 
-        public int Version { get; set; }
+        public int Version { get; set; } = 1;
 
         public string FileName { get; set; }
 
+        /// <summary>
+        /// Measured in bytes
+        /// </summary>
         public long FileSize { get; set; }
     }
 }
